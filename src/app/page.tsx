630bf7cf6dbcb9bb1e5563487ce59ad31621c329
Alex <alex.akten@outlook.com>
2024-04-02 14:38:37 +0200

    mt-10

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -41,7 +41,7 @@ export default function Landing() {
     <main className="relative flex tracking-tight flex-col items-center px-3 text-center bg-[#F9F9F9] text-zinc-900 sm:px-8 2xl:text-lg">
       <Navbar />
 
-      <section className="relative flex mt-48 w-full max-w-8xl flex-col justify-center items-center">
+      <section className="flex mt-48 w-full max-w-8xl flex-col justify-center items-center">
         <div className="px-6 py-1 text-black flex items-center gap-2 bg-white shadow-md rounded-full">
           {/* <div className="w-2.5 h-2.5 bg-black rounded-full"></div> */}
           <div className="flex ">
@@ -52,19 +52,19 @@ export default function Landing() {
           </div>
         </div>
         <h1 className="max-w-4xl mt-5 text-8xl tracking-tight">
-          We help you repay your debt.
+          Debt repayment simplified.
         </h1>
         {/* <h3 className="mt-8 max-w-md text-xl opacity-50">
           Stop worrying about your debt. Payable helps you set up a plan to pay
           back what you owe.
         </h3> */}
 
-        <button className="px-6 mt-8 py-2 rounded-full tracking-normal text-white bg-black hover:bg-purple ">
+        <button className="px-6 mt-10 py-2 rounded-full tracking-normal text-white bg-black hover:bg-purple ">
           Get started <span className={inter.className}>-&gt;</span>
         </button>
       </section>
 
-      <section className="grid m-20 grid-cols-4 w-full gap-4">
+      <section className="grid max-w-6xl m-20 grid-cols-4 w-full gap-4">
         {features.map((feature, index) => (
           <FeatureCard
             key={index}
